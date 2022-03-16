d236074e0112f1c631faa040a4f27c0c8ca6f726
Dudedu11 <muhamad.rafli.tif20@polban.ac.id.com>
2022-03-16 22:35:51 +0700

    oy

---
+++ my-project/src/api/grade-pendidikan/controllers/grade-pendidikan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  grade-pendidikan controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::grade-pendidikan.grade-pendidikan');
---
+++ my-project/src/api/grade-pendidikan/services/grade-pendidikan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * grade-pendidikan service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::grade-pendidikan.grade-pendidikan');
